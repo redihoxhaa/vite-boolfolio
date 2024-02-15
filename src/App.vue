dd1acd127dea0782ca860180df4368abff5ceda0
redihoxhaa <redi.hoxha1996@gmail.com>
2024-02-15 15:33:28 +0100

    feat: milestone 4

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,28 @@
 import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
 import AppFooter from './components/AppFooter.vue'
+import axios from 'axios'
 
 export default {
-  components: { AppHeader, AppMain, AppFooter }
+  data() {
+    return {
+      baseURL: 'http://127.0.0.1:8000',
+      URIs: {
+        projects: '/api/projects'
+      }
+    }
+  },
+  components: { AppHeader, AppMain, AppFooter },
+  methods: {
+    getProjects() {
+      axios.get(this.baseURL + this.URIs.projects).then(response => {
+        console.log(response);
+      })
+    }
+  },
+  mounted() {
+    this.getProjects();
+  }
 }
 </script>
 
